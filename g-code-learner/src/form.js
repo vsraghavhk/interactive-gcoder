928e9c9474cad9937fac9dceaf7d7292d64eaad4
Arman Rezaee <36311711+armanrze@users.noreply.github.com>
2020-11-30 05:47:44 -0600

    form layout

--- g-code-learner/src/form.js
+++ g-code-learner/src/form.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Form,
   Input,
   Button,
-  Radio,
   Select,
   Cascader,
   DatePicker,
   InputNumber,
   TreeSelect,
   Switch,
-} from 'antd';
+} from "antd";
 
 export const PrintParamForm = () => {
-  const [componentSize, setComponentSize] = useState('default');
+  const [componentSize, setComponentSize] = useState("default");
 
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
@@ -23,10 +22,10 @@ export const PrintParamForm = () => {
     <>
       <Form
         labelCol={{
-          span: 4,
+          span: 8,
         }}
         wrapperCol={{
-          span: 14,
+          span: 10,
         }}
         layout="horizontal"
         initialValues={{
@@ -35,62 +34,57 @@ export const PrintParamForm = () => {
         onValuesChange={onFormLayoutChange}
         size={componentSize}
       >
-        <Form.Item label="Form Size" name="size">
-          <Radio.Group>
-            <Radio.Button value="small">Small</Radio.Button>
-            <Radio.Button value="default">Default</Radio.Button>
-            <Radio.Button value="large">Large</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Input">
-          <Input />
-        </Form.Item>
-        <Form.Item label="Select">
+        <Form.Item label="Form Function">
           <Select>
-            <Select.Option value="demo">Demo</Select.Option>
+            <Select.Option value="1">Straight</Select.Option>
+            <Select.Option value="2">Spiral</Select.Option>
+            <Select.Option value="3">Wave</Select.Option>
+            <Select.Option value="4">Screw</Select.Option>
+            <Select.Option value="5">Random</Select.Option>
+            <Select.Option value="6">Shift</Select.Option>
+            <Select.Option value="7">Shift and turn</Select.Option>
           </Select>
         </Form.Item>
-        <Form.Item label="TreeSelect">
-          <TreeSelect
-            treeData={[
-              {
-                title: 'Light',
-                value: 'light',
-                children: [
-                  {
-                    title: 'Bamboo',
-                    value: 'bamboo',
-                  },
-                ],
-              },
-            ]}
-          />
-        </Form.Item>
-        <Form.Item label="Cascader">
-          <Cascader
-            options={[
-              {
-                value: 'zhejiang',
-                label: 'Zhejiang',
-                children: [
-                  {
-                    value: 'hangzhou',
-                    label: 'Hangzhou',
-                  },
-                ],
-              },
-            ]}
-          />
-        </Form.Item>
-        <Form.Item label="DatePicker">
-          <DatePicker />
-        </Form.Item>
-        <Form.Item label="InputNumber">
+        <Form.Item label="Edge Length">
           <InputNumber />
         </Form.Item>
-        <Form.Item label="Switch">
-          <Switch />
+        <Form.Item label="Z shift">
+          <InputNumber />
         </Form.Item>
+        <Form.Item label="Bed Temperature">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Nozzle Temperature">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Extrusion Rate">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Feed Rate">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Number of Sides">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Radius">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Number of Layers">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Wave Amplitude">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Randum Rate">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Extrusion Mode">
+          <Select>
+            <Select.Option value="1">Absolute</Select.Option>
+            <Select.Option value="2">Relative</Select.Option>
+          </Select>
+        </Form.Item>
+
         <Form.Item label="Button">
           <Button>Button</Button>
         </Form.Item>
@@ -98,6 +92,3 @@ export const PrintParamForm = () => {
     </>
   );
 };
-
-
-
